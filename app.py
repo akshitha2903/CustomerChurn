73617e1b7702c4e20891722442ef5c141234dc2a
Akshitha R <r.akshitha2903@gmail.com>
2024-05-13 23:32:48 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+import pickle
+import json
+from flask import Flask,request,app,jsonify,url_for,render_template
+import numpy as np
+import pandas as pd
+
+app=Flask(__name__)
+ranforest=pickle.load(open('ranforest.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=np.array(list(data.values())).reshape(1,-1)
+    output=ranforest.predict(new_data)
+    if output== 0:
+        result = "Not Churn"
+    else:
+        result = "Churn"
+    return jsonify({"Prediction": result})
+
+if __name__=="__main__":
+    app.run(debug=True)
